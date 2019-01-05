7bd02705e8ae510fa8e3ad94cdd67bd489e33179
Tammy Metz <tammy@tammymetz.com>
2019-01-05 15:22:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Write your code in this file!
 
 const currentUser = 'Grace Hopper';
-const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
-const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser + '!';
+//const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+//const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser + '!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
