f1a5a52530d36e8f4b4db802eef2c059f48970e5
Tammy Metz <tammy@tammymetz.com>
2019-01-05 15:19:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 
 const currentUser = 'Grace Hopper';
-const welcomeMessagetest = 'Welcome to Flatbook, ';
+const welcomeMessagetest = 'Welcome to Flatbook, ' + currentUser;
