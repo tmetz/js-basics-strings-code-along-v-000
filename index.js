a5ddd6504e632f22e9f51f440c5a73242efc5b54
Tammy Metz <tammy@tammymetz.com>
2019-01-05 15:22:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ const currentUser = 'Grace Hopper';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 //const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser + '!';
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser}`;
